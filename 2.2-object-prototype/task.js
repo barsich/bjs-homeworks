ea1a1189b13e94239c3f9797bc9f9ac0d6c2d793
barsich <barsicchan@gmail.com>
2021-01-12 20:55:45 +0500

    homework 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,26 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+  let str = this.replace(/\s+/g, "").toLowerCase();
+  let strReverse = str.split("").reverse().join("");
+  return (str === strReverse);
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let summ = 0;
+  let roundedAverage = 0;
+  if (marks.length !== 0) {
+    for (i = 0; i < marks.length; i++) {
+      summ += marks[i];
+    }
+    roundedAverage = Math.round(summ / marks.length);
+  }
+  return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    birthday = +(new Date(birthday));
+    let diff = now - birthday;
+    let age = (new Date(diff)).getFullYear() - 1970;
+    console.log(age);
+    return (age >= 18);
 }
